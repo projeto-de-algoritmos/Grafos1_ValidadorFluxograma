5ea831fd108b8c6b86d60d957826ec7d9f0b5aa5
Natan Santana <natan.santana.dev@gmail.com>
2023-09-20 08:52:37 -0300

    Otimização do algoritmo
    Co-authored-by: Clara Ribeiro <claramarcelinors@gmail.com>

--- topological_sort.js
+++ topological_sort.js
@@ -1,69 +1,92 @@
 const fluxoValido = [{
     nome: "APC",
     grau: 0,
-    dependencias: []
-},
-{
+    dependenciaDe: [{
+        nome: "OO",
+        index: 1
+    }]
+},{
     nome: "OO",
     grau: 1,
-    dependencias: ["APC"]
-},
-{
+    dependenciaDe: [{
+        nome: "MDS",
+        index: 2
+    }]
+},{
     nome: "MDS",
     grau: 1,
-    dependencias: ["OO"]
-},
-{
+    dependenciaDe: [{
+        nome: "EPS",
+        index: 5
+    }]
+},{
     nome: "PP",
     grau: 0,
-    dependencias: []
-},
-{
+    dependenciaDe: [{
+        nome: "EPS",
+        index: 5
+    }]
+},{
     nome: "TESTES",
     grau: 0,
-    dependencias: []
-},
-{
+    dependenciaDe: [{
+        nome: "EPS",
+        index: 5
+    }]
+},{
     nome: "EPS",
     grau: 3,
-    dependencias: ["MDS", "PP", "TESTES"]
+    dependenciaDe: []
 }];
 
 const fluxoInvalido = [{
     nome: "APC",
     grau: 1,
-    dependencias: ["MDS"] // Esse gera um paradoxo
-},
-{
+    dependenciaDe: [{
+        nome: "OO",
+        index: 1
+    }]
+},{
     nome: "OO",
     grau: 1,
-    dependencias: ["APC"]
-},
-{
+    dependenciaDe: [{
+        nome: "MDS",
+        index: 2
+    }]
+},{
     nome: "MDS",
     grau: 1,
-    dependencias: ["OO"]
-},
-{
+    dependenciaDe: [{
+        nome: "EPS",
+        index: 5
+    },{
+        nome: "APC",
+        index: 0
+    }]
+},{
     nome: "PP",
     grau: 0,
-    dependencias: []
-},
-{
+    dependenciaDe: [{
+        nome: "EPS",
+        index: 5
+    }]
+},{
     nome: "TESTES",
     grau: 0,
-    dependencias: []
-},
-{
+    dependenciaDe: [{
+        nome: "EPS",
+        index: 5
+    }]
+},{
     nome: "EPS",
     grau: 3,
-    dependencias: ["MDS", "PP", "TESTES"]
+    dependenciaDe: []
 }];
 
 const ordenacaoTopologica = (grafo) => {
     const ordemTopologica = [];
     const fila = grafo.filter(item => item.grau == 0); // Iniciação da fila com todos os nós com 0 dependencias O(n)
-    const array = grafo.filter(item => item.grau !== 0); // Retirada dos nós com 0 de dependência O(n)
+    const array = grafo;
     
     while (fila.length) {
         const noAtual = fila.shift(); // Pega o primeiro da fila dos nós com 0 dependencia
@@ -71,27 +94,26 @@ const ordenacaoTopologica = (grafo) => {
             fila.length = 0;
         } else {
             ordemTopologica.push(noAtual); // Adiciona na array da ordem topologica
-            for (let index = 0, len = array.length; index < len; index++) { // Procurar todos os nós com dependência
-                const materia = array[index];
-                const possuiDependenciaNoAtual = materia.dependencias.find(item => {
-                    return item == noAtual.nome;
-                });
-                if (possuiDependenciaNoAtual) {
-                    array[index].grau--; // Diminuir grau da materia com dependência
-                    if (array[index].grau == 0) {
-                        fila.push(array[index]); // Se grau for zero, vai para a fila
-                    }
+
+            for (let i = 0, len = noAtual.dependenciaDe.length; i < len; i++) { // Atualização do grau dos vizinhos
+                const index = noAtual.dependenciaDe[i].index;
+                
+                array[index].grau--;
+                if (array[index].grau == 0) {
+                    fila.push(array[index]); // Se grau for zero, vai para a fila
                 }
             }
+
         }
     }
 
-    return ordemTopologica;
+    return array.length == ordemTopologica.length; // Se a array final de ordem topológica não tiver o mesmo tamanho, é porque o fluxo não é válido e possui um ciclo
+    
 }
 
 const resultadoValido = ordenacaoTopologica(fluxoValido);
 const resultadoInvalido = ordenacaoTopologica(fluxoInvalido);
 console.log("Resultado Válido:")
 console.log(resultadoValido);
-console.log("Resultado Inválido: ")
+console.log("Resultado Inválido:")
 console.log(resultadoInvalido);
